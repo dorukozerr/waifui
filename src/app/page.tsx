a4ac6465d12c8f2cc0c7225572bbf51c9018f787
Doruk Özer <dorukozer@protonmail.com>
2024-12-10 06:07:05 +0300

    fix: ssr error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { H1 } from "tamagui";
 import { HomePage } from "@/views/home-page";
 
-const Page = () => (
-  <>
-    <H1>Hello</H1>
-    <HomePage />;
-  </>
-);
+const Page = () => <HomePage />;
 
 export default Page;
