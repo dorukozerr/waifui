ad83321a53ee6586d8dbf242bf158449854adaab
Doruk Özer <dorukozer@protonmail.com>
2024-12-10 05:00:00 +0300

    test: mobile overflow testing

--- src/views/home-page.tsx
+++ src/views/home-page.tsx
@@ -1,9 +1,42 @@
 "use client";
 
 import { ScrollView, H1 } from "tamagui";
+import { Button } from "@/components/waifui/button";
 
 export const HomePage = () => (
   <ScrollView bc="$red11" h="100%">
     <H1>Hello from waifui</H1>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
+    <Button>Test</Button>
   </ScrollView>
 );
